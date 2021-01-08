f9636cd306531f63b2c6a04c20d585e3be7cc481
Jolie Hobloss <jolieshobloss@gmail.com>
2021-01-08 11:16:37 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -11,13 +11,28 @@ export default class ImageSlider extends React.Component {
   }
 
     render() {
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+>>>>>>> 35fea1b999540f77df5691c61dc5c5160ceeb44f
       return (
         <div>
         I am on slide {this.state.currentSlideIndex}
+=======
+        <div>
+        'I am on slide {this.state.currentSlideIndex}'
+>>>>>>> 47b9c797095eef0fba995fa3be354d10968a8dd8
         </div>
       )
       
     }
   }
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+  
+>>>>>>> 35fea1b999540f77df5691c61dc5c5160ceeb44f
 
+=======
+>>>>>>> 47b9c797095eef0fba995fa3be354d10968a8dd8
